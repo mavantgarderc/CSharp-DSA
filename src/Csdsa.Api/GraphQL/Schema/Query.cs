333ce5b1ee6e410b04ef1953dcf4804902a69cb7
mavantgarderc <manihabibinava@gmail.com>
2025-07-29 16:53:31 +0330

    feat: GraphQL Query added

---
+++ src/Csdsa.Api/GraphQL/Schema/Query.cs
@@ -0,0 +1,11 @@
+// using Application.Users.Queries.GetUserById;
+// using Domain.Entities;
+// using Csdsa.Domain.Models.Common;
+//
+// public class Query
+// {
+//     public async Task<User?> GetUserById([Service] ISender sender, Guid id)
+//         => await sender.Send(new GetUserByIdQuery(id));
+// }
+
+// uncomment when ready to activate
