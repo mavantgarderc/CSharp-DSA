ba5306d8c26238747bcd7b233573d1265aabc4ae
mavantgarderc <manihabibinava@gmail.com>
2025-07-29 16:53:49 +0330

    feat: SignalR Notification added

---
+++ src/Csdsa.Api/SignalR/Hubs/NotificationsHub.cs
@@ -0,0 +1,14 @@
+// using Microsoft.AspNetCore.SignalR;
+//
+// public class NotificationsHub : Hub
+// {
+//     public async Task SendMessage(string message)
+//     {
+//         await Clients.All.SendAsync("ReceiveMessage", message);
+//     }
+// }
+
+// uncomment whenever need to activate
+
+// add to Program.cs whenever ready to activate
+// app.MapHub<NotificationsHub>("/hubs/notifications");
