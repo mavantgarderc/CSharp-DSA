3c4994cadccf837d22a4a2a8977e133095f4fa07
mavantgarderc <manihabibinava@gmail.com>
2025-10-06 00:34:18 +0330

    fix(login): duplicate logout command

--- src/Csdsa.Application/Services/Auth/Login/LoginCommand.cs
+++ src/Csdsa.Application/Services/Auth/Login/LoginCommand.cs
@@ -7,7 +7,15 @@ namespace Csdsa.Application.Services.Auth.Login;
 public class LoginCommand : IRequest<OperationResult<AuthResponse>>
 {
     public string Email { get; set; } = string.Empty;
+
     public string Password { get; set; } = string.Empty;
+
     public string IpAddress { get; set; } = string.Empty;
-    public string UserAgent { get; set; } = string.Empty;
+
+    // public LoginCommand(string email, string password, string ipAddress)
+    // {
+    //     Email = email ?? throw new ArgumentNullException(nameof(email));
+    //     Password = password ?? throw new ArgumentNullException(nameof(password));
+    //     IpAddress = ipAddress ?? throw new ArgumentNullException(nameof(ipAddress));
+    // }
 }
